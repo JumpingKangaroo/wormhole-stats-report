b2dfefb68f8535930c3affba4d651fdde08e9da9
Chris Lambertson <lambertsonchris@gmail.com>
2019-03-21 01:51:49 -0400

    Finished fetching of ccp info. Implemented rudimentary calculation of total killed and lost

--- fetch_jsons.py
+++ fetch_jsons.py
@@ -13,6 +13,12 @@ def read_zkb_json(corpID):
     data = json.load(json_file)
   return data
 
+def read_json(corporationID, mailType):
+  with open(str(corporationID) + '_' + mailType + ".json") as json_file:
+    data = json.load(json_file)
+  return data
+
+
 def get_url(params, pageNumber):
   url = 'https://zkillboard.com/api/'
   for param in params:
@@ -32,9 +38,18 @@ def fetch_ccp_kill_info(zkb_json):
   for kill in zkb_json:
     baseUrl = "https://esi.evetech.net/dev/killmails/"
     url = baseUrl + str(kill["killmail_id"]) + "/" + kill["zkb"]["hash"] + "/"
-    if DEBUG: print("Printing:", i, "of", size, "\tccp url:", url)
-    r = requests.get(url, timeout=1, headers=headers)
-    ccp_json[kill["killmail_id"]] = r.json()
+    if DEBUG: print("Getting:", i, "of", size, "\tccp url:", url)
+    success = False 
+    while not success:
+      try:
+        r = requests.get(url, headers=headers)
+        ccp_json[int(kill["killmail_id"])] = r.json()
+        success = True 
+      except:
+        print ("Failed, retrying")
+        pass
+    i += 1
+    time.sleep(0.3)
   return ccp_json
 
 def fetch_corporation_kills_zkill(corpID, month):
@@ -43,7 +58,7 @@ def fetch_corporation_kills_zkill(corpID, month):
   urlParams = [ 'corporationID', str(corpID), 'month', month, 'page' ]
   if DEBUG: print("requesting page:", pageNumber) 
   url = get_url(urlParams, pageNumber)
-  r = requests.get(url, headers=headers, timeout=1)
+  r = requests.get(url, headers=headers)
   r = r.json()
   json_zkill = r
   if DEBUG: print("length of r:", len(r)) 
@@ -58,12 +73,15 @@ def fetch_corporation_kills_zkill(corpID, month):
     if DEBUG: print("length of r:", len(r)) 
   if DEBUG: print("Final length", len(json_zkill))
   if DEBUG: print("Now fetching from CCP servers")
-  json_ccp = fetch_ccp_kill_info(json_zkill)
-  if DEBUG: print('Now saving to file')
-  write_file(corpID, json_ccp, "ccp")
   write_file(corpID, json_zkill, "zkill")
+  if DEBUG: print('Now saving to file')
+  return json_zkill
 
 
 
-fetch_corporation_kills_zkill(98504356, 2)
+# fetch_corporation_kills_zkill(98504356, 1) # Mcav
+# fetch_corporation_kills_zkill(1705300610, 1) 
+json_zkill = read_json(1705300610, "zkill")
+json_ccp = fetch_ccp_kill_info(json_zkill)
+write_file(1705300610, json_ccp, "ccp")
 
--- process_kills.py
+++ process_kills.py
@@ -4,26 +4,52 @@ import requests
 # This script takes in the json filename, and processes it to 
 # count kills.
 
+DEBUG = True
+
 def read_json(corporationID, mailType):
-  with open(corporationID + '_' + mailType + ".json") as json_file:
+  with open(str(corporationID) + '_' + mailType + ".json") as json_file:
     data = json.load(json_file)
   return data
 
 def calculate_efficiency(zkb_json, ccp_json, corporationID):
-    killsSum = 0
-    lossesSum = 0
+    killsSum = 0.0
+    killCount = 0
+    lossesSum = 0.0
+    lossesCount = 0
     for kill in zkb_json:
+        # If its an NPC kill, skip
+        # if kill["zkb"]["npc"] or kill["zkb"]["awox"]:
+        #   continue
         # check if its a loss, else its a kill
-        if ccp_json[kill["killmail_id"]]["victim"]["corporation_id"] == corporationID:
+        if str(ccp_json[str(kill["killmail_id"])]["victim"]["corporation_id"]) == str(corporationID):
+            # if DEBUG: print (kill["killmail_id"], "\tis a loss worth \t{:,}".format(kill["zkb"]["totalValue"]), "\tKill url:", "https://zkillboard.com/kill/" + str(kill["killmail_id"]) + "/")
             lossesSum += kill["zkb"]["totalValue"]
+            lossesCount += 1
         else:
+            if DEBUG: print (kill["killmail_id"], "\tis a kill worth \t{:,}".format(kill["zkb"]["totalValue"]), "\tKill url:", "https://zkillboard.com/kill/" + str(kill["killmail_id"]) + "/")
             killsSum += kill["zkb"]["totalValue"]
-    efficiency = killsSum / lossesSum
-    return efficiency 
+            killCount += 1
+    if DEBUG: print ("Kill count:", killCount)
+    if DEBUG: print ("Loss count:", lossesCount)
+    if DEBUG: print ("zkb count: {:,} ccp count: {:,}".format(len(zkb_json), len(ccp_json)))
+    print("Kills sum: {:,}".format(killsSum))
+    print("Losses sum: {:,}".format(lossesSum))
+    return (killsSum, lossesSum)
 
-# Returns a dict with the 
-def process_kills(corporationID):
-    zkill_json = read_json(corporationID, "zkill")
-    ccp_json = read_json(corporationID, "ccp")
+# Takes in a list of corporation IDs, and returns a dict of the stats
+def process_kills(corporationIDs):
     statsDict = {}
-    statsDict["name"] = corporationID
+    for corpID in corporationIDs:
+      zkill_json = read_json(corpID, "zkill")
+      ccp_json = read_json(corpID, "ccp")
+      statsDict[corpID] = {}
+      statsDict[corpID]["corporationID"] = corpID
+      killsSum, lossesSum = calculate_efficiency(zkill_json, ccp_json, corpID)
+      statsDict[corpID]["killsSum"] = killsSum
+      statsDict[corpID]["lossesSum"] = lossesSum
+    return statsDict
+
+# result = process_kills([98504356]) # mcav
+result = process_kills([1705300610]) # mcav
+# print("Kills sum: {:,}".format(result[98504356]["killsSum"]))
+# print("Losses sum: {:,}".format(result[98504356]["lossesSum"]))
