495444db151e3b8905707c18f0fd127d55acf45c
Chris <lambertsonchris@gmail.com>
2019-03-23 13:50:53 -0400

    More work on inserting json kill data into proper database

--- save_kills_to_db.py
+++ save_kills_to_db.py
@@ -1,8 +1,11 @@
 import sqlite3 
 from sqlite3 import Error 
 from db_controller import mailDB
+from fetch_jsons import read_json
 
+def json_to_database(corporationID):
+  zkb_json = read_json(corporationID, "zkill")
+  ccp_json = read_json(corporationID, "ccp")
 
 if __name__ == '__main__':
   killmails_db = mailDB("killmails.db")
-  
